8a7c213d8338ea4ee14714e4be7dea000aeb057e
SlimRunner <dv.polygon@gmail.com>
2023-06-12 16:51:16 -0700

    add functions to rational class

--- src/arithmetic/RationalNumber.ts
+++ src/arithmetic/RationalNumber.ts
@@ -22,4 +22,34 @@ export class RationalNumber {
     this.denominator = denominator / commonDenom;
   }
 
+  add(rhs: RationalNumber) {
+    const commonDenom = gcd(this.denominator, rhs.denominator);
+    const leastMult = this.denominator * rhs.denominator / commonDenom;
+    const a = this.numerator * rhs.denominator / commonDenom;
+    const b = rhs.numerator * this.denominator / commonDenom;
+    return new RationalNumber(a + b, leastMult);
+  }
+
+  subtract(rhs: RationalNumber) {
+    const commonDenom = gcd(this.denominator, rhs.denominator);
+    const leastMult = this.denominator * rhs.denominator / commonDenom;
+    const a = this.numerator * rhs.denominator / commonDenom;
+    const b = rhs.numerator * this.denominator / commonDenom;
+    return new RationalNumber(a - b, leastMult);
+  }
+
+  multiply(rhs: RationalNumber) {
+    return new RationalNumber(this.numerator * rhs.numerator, this.denominator * rhs.denominator);
+  }
+
+  divide(rhs: RationalNumber) {
+    return new RationalNumber(this.numerator * rhs.denominator, this.denominator * rhs.numerator);
+  }
+
+  fixSigns() {
+    const negative = this.numerator < 0 !== this.denominator < 0;
+    const a = Math.abs(this.numerator) * (negative? -1: 1);
+    const b = Math.abs(this.denominator);
+    return new RationalNumber(a, b);
+  }
 }
