02f979e48cc5248a7bf42fce0da6e79c28dbb0f3
SlimRunner <dv.polygon@gmail.com>
2023-09-20 11:53:32 -0700

    add comparison functions

--- src/utilities/comparison.ts
+++ src/utilities/comparison.ts
@@ -1,5 +1,21 @@
 import { Arithmetic, Comparable, Congruent } from "../interfaces";
 
+export function isEqual<T extends Comparable<T>>(lhs: T, rhs: T): boolean {
+  return lhs.compare(rhs) === 0;
+}
+
+export function isGreaterThan<T extends Comparable<T>>(lhs: T, rhs: T): boolean {
+  return lhs.compare(rhs) > 0; // lhs - rhs > 0 <=> lhs > rhs
+}
+
+export function isLessThan<T extends Comparable<T>>(lhs: T, rhs: T): boolean {
+  return lhs.compare(rhs) < 0; // lhs - rhs < 0 <=> lhs < rhs
+}
+
+export function inBetween<T extends Comparable<T>>(mid: T, lhs: T, rhs: T): boolean {
+  return isLessThan(lhs, mid) && isLessThan(mid, rhs);
+}
+
 export function min<T extends Comparable<T>>(...values: T[]) {
   if (values.length === 0) {
     throw SyntaxError("min function must have at least one parameter");
